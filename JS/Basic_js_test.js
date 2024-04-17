660ca8580a1e2edff221c42ff3c37df0d9989bb4
akasia714 <ss090007@naver.com>
2024-04-17 14:23:34 +0900

    4.17

--- JS/Basic_js_test.js
+++ JS/Basic_js_test.js
@@ -1,5 +1,6 @@
-document.getElementById("search_btn").addEventListener('click', search_message);
-
-function search_message(){
-    alert("검색을 수행합니다.");
+const search_message = () => {
+    const c = '검색을 수행합니다.';
+    alert(c);
 }
+
+document.getElementById("search_btn").addEventListener('click', search_message);
